e5624b3470889dee791814939aaae3c54844828f
Ingrid <ingrid.oa@gmail.com>
2024-05-30 19:23:54 -0400

    Cambio asociado a los graficos de torta

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
 $("form").submit(function (event) {
   event.preventDefault();
 
-  
   let inputValue = $("#input-buscar").val();
   let regex = /^[1-9]\d*$/;
-  
+
   if (regex.test(inputValue)) {
     // Si el valor es un número positivo, realizar la solicitud AJAX
     let requestConfig = {
@@ -17,18 +16,34 @@ $("form").submit(function (event) {
         let postCard = `<div class="card mb-3 m-auto" style="max-width: 540px;">
           <div class="row g-0">
             <div class="col-md-4">
-              <img src="${post.image.url}" alt="${post.name}" class="w-100">
+              <img src="${post.image.url}" alt="${post.name}" class="w-100 h-100">
             </div>
             <div class="col-md-8">
               <div class="card-body">
                 <h5 class="card-title">${post.name}</h5> 
-                <p class="card-text">Parientes: ${post.connections.relatives}</p>
+                <p class="card-text">Parientes: ${
+                  post.connections.relatives
+                }</p>
                 <p class="card-text">Ocupación: ${post.work.occupation}</p>
-                <p class="card-text">Publicado por: ${post.biography.publisher}</p>
-                <p class="card-text">Aliansa: ${post.connections["group-affiliation"]}</p>
-                <p class="card-text">Altura: ${Array.isArray(post.appearance.height) ? post.appearance.height.join(' / ') : post.appearance.height}</p>
-                <p class="card-text">Peso: ${Array.isArray(post.appearance.weight) ? post.appearance.weight.join(' / ') : post.appearance.weight}</p>            
-                <p class="card-text"><small class="text-body-secondary">Género: ${post.appearance.gender}</small></p>
+                <p class="card-text">Publicado por: ${
+                  post.biography.publisher
+                }</p>
+                <p class="card-text">Aliansa: ${
+                  post.connections["group-affiliation"]
+                }</p>
+                <p class="card-text">Altura: ${
+                  Array.isArray(post.appearance.height)
+                    ? post.appearance.height.join(" / ")
+                    : post.appearance.height
+                }</p>
+                <p class="card-text">Peso: ${
+                  Array.isArray(post.appearance.weight)
+                    ? post.appearance.weight.join(" / ")
+                    : post.appearance.weight
+                }</p>            
+                <p class="card-text"><small class="text-body-secondary">Género: ${
+                  post.appearance.gender
+                }</small></p>
 
               </div>
             </div>
@@ -41,48 +56,43 @@ $("form").submit(function (event) {
         let estadistica = {
           durabilidad: parseInt(post.powerstats.durability),
           inteligencia: parseInt(post.powerstats.intelligence),
-          fuerza: parseInt(post.powerstats.strength), 
-          rapidez: parseInt(post.powerstats.speed), 
+          fuerza: parseInt(post.powerstats.strength),
+          rapidez: parseInt(post.powerstats.speed),
           combate: parseInt(post.powerstats.combat),
-          poder: parseInt(post.powerstats.power), 
+          poder: parseInt(post.powerstats.power),
+        };
 
-        } 
+          var options = {
+	title: {
+		text: "Estadísticas de los Superhero"
+	},
+	subtitles: [{
+		text: `${post.name}`
+	}],
+  	theme: "light2",
+	animationEnabled: true,
+	data: [{
+		type: "pie",
+		startAngle: 40,
+		toolTipContent: "<b>{label}</b>: {y}%",
+		showInLegend: "true",
+		legendText: "{label}",
+		indexLabelFontSize: 16,
+		indexLabel: "{label} - {y}%",
+		dataPoints: [
+			{ y: estadistica.durabilidad, label: "durabilidad" },
+			{ y: estadistica.inteligencia, label: "inteligencia" },
+			{ y: estadistica.fuerza, label: "Fuerza" },
+			{ y: estadistica.rapidez, label: "Rapidez" },
+			{ y: estadistica.poder, label: "Power" },
+			{ y: estadistica.combate, label: "Combate" }
+			
+		]
+	}]
+};
+$("#chartContainer").CanvasJSChart(options);
 
-        const stact = [
-          { y: parseInt(post.powerstats.durability), label: "durabilidad" },
-          { y: parseInt(post.powerstats.intelligence), label:"inteligencia"},
-          { y: parseInt(post.powerstats.strength), label: "fuerza"},
-          { y: parseInt(post.powerstats.speed), label: "rapidez"},
-          { y: parseInt(post.powerstats.combat), label: "combate"},
-          { y: parseInt(post.powerstats.power), label: "poder"}
-          
-        ]
-
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-          theme: "light2", // "light1", "light2", "dark1", "dark2"
-          exportEnabled: true,
-          animationEnabled: true,
-          title: {
-            text: "Desktop Browser Market Share in 2016"
-          },
-          data: [{
-            type: "pie",
-            startAngle: 25,
-            toolTipContent: "<b>{label}</b>: {y}%",
-            showInLegend: "true",
-            legendText: "{label}",
-            indexLabelFontSize: 16,
-            indexLabel: "{label} - {y}%",
-            dataPoints: stact,
-        
-          }]
-        });
-        chart.render();
-        
-        }
-
-      },
+},
       error: function (error) {
         console.log("¡Ha ocurrido un error!", error);
         alert("¡PELIGRO! Ha ocurrido un error.");
